f08747df747ecccd0203bbebed4279166c008c96
Lucky Verma <luckyv1@umbc.edu>
2022-10-31 01:39:24 -0400

    prepare csv data init

--- prepare_resteaze.py
+++ prepare_resteaze.py
@@ -7,30 +7,35 @@ import shutil
 import pyedflib
 import numpy as np
 import pandas as pd
+import datetime
 
-from sleepstage import stage_dict
+from sleepstage import resteaze_stage_dict
 from logger import get_logger
 
+from scipy.signal import butter, sosfilt, sosfreqz
 
-# Have to manually define based on the dataset
-ann2label = {
-    "Sleep stage W": 0,
-    "Sleep stage 1": 1,
-    "Sleep stage 2": 2,
-    "Sleep stage 3": 3, "Sleep stage 4": 3, # Follow AASM Manual
-    "Sleep stage R": 4,
-    "Sleep stage ?": 6,
-    "Movement time": 5
-}
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    sos = butter(order, [low, high], analog=False, btype='band', output='sos')
+    return sos
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    sos = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = sosfilt(sos, data)
+    return y
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str, default="./data/sleepedf/sleep-cassette",
-                        help="File path to the Sleep-EDF dataset.")
-    parser.add_argument("--output_dir", type=str, default="./data/sleepedf/sleep-cassette/eeg_fpz_cz",
+    parser.add_argument("--data_dir", type=str, default="./prof_data/resteaze",
+                        help="File path to the resteaze dataset.")
+    parser.add_argument("--output_dir", type=str, default="./prof_data/resteaze/ppg_ledgreen",
                         help="Directory where to save outputs.")
-    parser.add_argument("--select_ch", type=str, default="EEG Fpz-Cz",
+    parser.add_argument("--select_ch", type=str, default="LedGreen",
                         help="Name of the channel in the dataset.")
     parser.add_argument("--log_file", type=str, default="info_ch_extract.log",
                         help="Log file.")
@@ -52,88 +57,56 @@ def main():
     select_ch = args.select_ch
 
     # Read raw and annotation from EDF files
-    psg_fnames = glob.glob(os.path.join(args.data_dir, "*PSG.edf"))
-    ann_fnames = glob.glob(os.path.join(args.data_dir, "*Hypnogram.edf"))
-    psg_fnames.sort()
-    ann_fnames.sort()
-    psg_fnames = np.asarray(psg_fnames)
-    ann_fnames = np.asarray(ann_fnames)
+    ppg_fnames = glob.glob(os.path.join(args.data_dir, "*.csv"))
+    ppg_fnames.sort()
+    ppg_fnames = np.asarray(ppg_fnames)
 
-    for i in range(len(psg_fnames)):
+    for i in range(len(ppg_fnames)):
 
         logger.info("Loading ...")
-        logger.info("Signal file: {}".format(psg_fnames[i]))
-        logger.info("Annotation file: {}".format(ann_fnames[i]))
+        logger.info("Signal file: {}".format(ppg_fnames[i]))
 
-        psg_f = pyedflib.EdfReader(psg_fnames[i])
-        ann_f = pyedflib.EdfReader(ann_fnames[i])
+        ppg_df = pd.read_csv(ppg_fnames[i], sep=',').dropna()
 
-        assert psg_f.getStartdatetime() == ann_f.getStartdatetime()
-        start_datetime = psg_f.getStartdatetime()
+        start_datetime = datetime.datetime.fromtimestamp(ppg_df['unixTimes'][0] / 1000)
         logger.info("Start datetime: {}".format(str(start_datetime)))
 
-        file_duration = psg_f.getFileDuration()
+        file_duration = datetime.datetime.fromtimestamp((ppg_df['unixTimes'][len(ppg_df)-1] - ppg_df['unixTimes'][0]) / 1000)
         logger.info("File duration: {} sec".format(file_duration))
-        epoch_duration = psg_f.datarecord_duration
-        if psg_f.datarecord_duration == 60: # Fix problems of SC4362F0-PSG.edf, SC4362FC-Hypnogram.edf
-            epoch_duration = epoch_duration / 2
-            logger.info("Epoch duration: {} sec (changed from 60 sec)".format(epoch_duration))
-        else:
-            logger.info("Epoch duration: {} sec".format(epoch_duration))
+        epoch_duration = 30
+        logger.info("Epoch duration: {} sec".format(epoch_duration))
 
         # Extract signal from the selected channel
-        ch_names = psg_f.getSignalLabels()
-        ch_samples = psg_f.getNSamples()
-        select_ch_idx = -1
-        for s in range(psg_f.signals_in_file):
-            if ch_names[s] == select_ch:
-                select_ch_idx = s
-                break
-        if select_ch_idx == -1:
-            raise Exception("Channel not found.")
-        sampling_rate = psg_f.getSampleFrequency(select_ch_idx)
+        ch_samples = len(ppg_df[select_ch])
+
+        sampling_rate = 25
         n_epoch_samples = int(epoch_duration * sampling_rate)
-        signals = psg_f.readSignal(select_ch_idx).reshape(-1, n_epoch_samples)
+        
+        # apply bandpass filter
+
+        fs = 25
+        lowcut = 0.35
+        highcut = 5.0
+
+        pro_ppg = butter_bandpass_filter(ppg_df[select_ch], lowcut, highcut, fs, order=5)
+
+        signals = pro_ppg[:-(ppg_df.shape[0] % n_epoch_samples)].reshape(-1, n_epoch_samples)
         logger.info("Select channel: {}".format(select_ch))
-        logger.info("Select channel samples: {}".format(ch_samples[select_ch_idx]))
+        logger.info("Select channel samples: {}".format(ch_samples))
         logger.info("Sample rate: {}".format(sampling_rate))
 
         # Sanity check
-        n_epochs = psg_f.datarecords_in_file
-        if psg_f.datarecord_duration == 60: # Fix problems of SC4362F0-PSG.edf, SC4362FC-Hypnogram.edf
-            n_epochs = n_epochs * 2
-        assert len(signals) == n_epochs, f"signal: {signals.shape} != {n_epochs}"
+        n_epochs = signals.shape[0]
 
         # Generate labels from onset and duration annotation
         labels = []
-        total_duration = 0
-        ann_onsets, ann_durations, ann_stages = ann_f.readAnnotations()
-        for a in range(len(ann_stages)):
-            onset_sec = int(ann_onsets[a])
-            duration_sec = int(ann_durations[a])
-            ann_str = "".join(ann_stages[a])
 
-            # Sanity check
-            assert onset_sec == total_duration
+        sleep_state = ppg_df['sleep_state'][:-(ppg_df.shape[0] % n_epoch_samples)]
+        i = 0
+        for j in range(n_epochs):
+            labels.append(round(sum(sleep_state[i:j])))
+            i = j
 
-            # Get label value
-            label = ann2label[ann_str]
-
-            # Compute # of epoch for this stage
-            if duration_sec % epoch_duration != 0:
-                logger.info(f"Something wrong: {duration_sec} {epoch_duration}")
-                raise Exception(f"Something wrong: {duration_sec} {epoch_duration}")
-            duration_epoch = int(duration_sec / epoch_duration)
-
-            # Generate sleep stage labels
-            label_epoch = np.ones(duration_epoch, dtype=np.int) * label
-            labels.append(label_epoch)
-
-            total_duration += duration_sec
-
-            logger.info("Include onset:{}, duration:{}, label:{} ({})".format(
-                onset_sec, duration_sec, label, ann_str
-            ))
         labels = np.hstack(labels)
 
         # Remove annotations that are longer than the recorded signals
@@ -145,7 +118,7 @@ def main():
 
         # Select only sleep periods
         w_edge_mins = 30
-        nw_idx = np.where(y != stage_dict["W"])[0]
+        nw_idx = np.where(y != resteaze_stage_dict["W"])[0]
         start_idx = nw_idx[0] - (w_edge_mins * 2)
         end_idx = nw_idx[-1] + (w_edge_mins * 2)
         if start_idx < 0: start_idx = 0
@@ -156,26 +129,26 @@ def main():
         y = y[select_idx]
         logger.info("Data after selection: {}, {}".format(x.shape, y.shape))
 
-        # Remove movement and unknown
-        move_idx = np.where(y == stage_dict["MOVE"])[0]
-        unk_idx = np.where(y == stage_dict["UNK"])[0]
-        if len(move_idx) > 0 or len(unk_idx) > 0:
-            remove_idx = np.union1d(move_idx, unk_idx)
-            logger.info("Remove irrelavant stages")
-            logger.info("  Movement: ({}) {}".format(len(move_idx), move_idx))
-            logger.info("  Unknown: ({}) {}".format(len(unk_idx), unk_idx))
-            logger.info("  Remove: ({}) {}".format(len(remove_idx), remove_idx))
-            logger.info("  Data before removal: {}, {}".format(x.shape, y.shape))
-            select_idx = np.setdiff1d(np.arange(len(x)), remove_idx)
-            x = x[select_idx]
-            y = y[select_idx]
-            logger.info("  Data after removal: {}, {}".format(x.shape, y.shape))
+        # # Remove movement and unknown
+        # move_idx = np.where(y == stage_dict["MOVE"])[0]
+        # unk_idx = np.where(y == stage_dict["UNK"])[0]
+        # if len(move_idx) > 0 or len(unk_idx) > 0:
+        #     remove_idx = np.union1d(move_idx, unk_idx)
+        #     logger.info("Remove irrelavant stages")
+        #     logger.info("  Movement: ({}) {}".format(len(move_idx), move_idx))
+        #     logger.info("  Unknown: ({}) {}".format(len(unk_idx), unk_idx))
+        #     logger.info("  Remove: ({}) {}".format(len(remove_idx), remove_idx))
+        #     logger.info("  Data before removal: {}, {}".format(x.shape, y.shape))
+        #     select_idx = np.setdiff1d(np.arange(len(x)), remove_idx)
+        #     x = x[select_idx]
+        #     y = y[select_idx]
+        #     logger.info("  Data after removal: {}, {}".format(x.shape, y.shape))
 
         # Save
-        filename = ntpath.basename(psg_fnames[i]).replace("-PSG.edf", ".npz")
+        filename = ntpath.basename(ppg_fnames[i]).replace(".csv", ".npz")
         save_dict = {
-            "x": x, 
-            "y": y, 
+            "x": x,
+            "y": y,
             "fs": sampling_rate,
             "ch_label": select_ch,
             "start_datetime": start_datetime,
@@ -191,4 +164,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- sleepstage.py
+++ sleepstage.py
@@ -19,6 +19,17 @@ stage_dict = {
     "UNK": UNK,
 }
 
+resteaze_stage_dict = {
+    "WK": 0,
+    "N1": 1,
+    "N2": 2,
+    "N3": 3,
+    "REM": 4,
+    "MOVE": MOVE,
+    "UNK": -1,
+}
+
+
 class_dict = {
     W: "W",
     N1: "N1",
