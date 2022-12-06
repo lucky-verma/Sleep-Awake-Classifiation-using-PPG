81f74387c9c9ad2fbc336b60a64b4e3d9514fcf1
lucky verma <thinkersloop@gmail.com>
2022-12-06 12:21:18 -0500

    segmented analysis and streamlit init

---
+++ pages/peaks.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import heartpy as hp
+
+
---
+++ streamlit_app.py
@@ -0,0 +1,40 @@
+import pyedflib
+import os
+import shutil
+import glob
+import datetime
+import time
+import pandas as pd
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+
+from numpy import mean, sqrt, square, arange
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import figure
+
+# hide streamlit warnings
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+# hide streamlit logo
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# streamlit page config
+st.set_page_config(
+    page_title="Heart Rate Variability",
+    page_icon=":heartbeat:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# streamlit title
+st.title("Heart Rate Variability")
+
+# streamlit sidebar
